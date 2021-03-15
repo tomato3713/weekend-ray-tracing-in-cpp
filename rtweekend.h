da6684905dcea7106da422afb79cf58fb768637f
Taichi Watanabe <tomato3713@outlook.com>
2021-03-15 23:47:36 +0900

    Add random number generate function

--- rtweekend.h
+++ rtweekend.h
@@ -5,9 +5,18 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
+
+// TODO: Not sufficient randomness
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
+}
 
 // utility functions
 inline double degrees_to_raians(double degrees) {
