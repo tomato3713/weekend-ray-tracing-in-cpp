1813414798604b8a032e701a224150dccfdd73d1
Taichi Watanabe <tomato3713@outlook.com>
2021-03-17 00:34:04 +0900

    gamma correction

--- color.h
+++ color.h
@@ -13,10 +13,11 @@ void write_color(std::ostream &out, color pixel_color,
   auto g = pixel_color.y();
   auto b = pixel_color.z();
 
+  // do gamma correction in gamma = 2.0
   auto scale = 1.0 / samples_per_pixel;
-  r *= scale;
-  g *= scale;
-  b *= scale;
+  r = sqrt(scale * r);
+  g = sqrt(scale * g);
+  b = sqrt(scale * b);
 
   out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
       << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
