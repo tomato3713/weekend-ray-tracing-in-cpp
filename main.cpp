3e250e8f5854b1aa9a05b06cdee80bae6ed41830
Taichi Watanabe <tomato3713@outlook.com>
2021-03-17 00:29:41 +0900

    add diffuse ray

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
+#include "camera.h"
 #include "color.h"
 #include "hittable_list.h"
 #include "rtweekend.h"
 #include "sphere.h"
-#include "camera.h"
 
 #include <iostream>
 
-color ray_color(const ray &r, const hittable &world) {
+color ray_color(const ray &r, const hittable &world, int depth) {
   hit_record rec;
+
+  // stop tracing.
+  if (depth <= 0)
+      return color(0.0);
+
   if (world.hit(r, 0, infinity, rec)) {
-    return 0.5 * (rec.normal + color(1));
+    point3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
   }
 
   vec3 unit_direction = unit_vector(r.direction());
@@ -22,6 +28,7 @@ int main(int argc, char **argv) {
   const int image_width = 384;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
   const int samples_per_pixel = 100;
+  const int max_depth = 100;
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -43,7 +50,7 @@ int main(int argc, char **argv) {
         auto u = (i + random_double()) / (image_width - 1);
         auto v = (j + random_double()) / (image_height - 1);
         ray r = cam.get_ray(u, v);
-        pixel_color += ray_color(r, world);
+        pixel_color += ray_color(r, world, max_depth);
       }
       write_color(std::cout, pixel_color, samples_per_pixel);
     }
--- vec3.h
+++ vec3.h
@@ -1,5 +1,6 @@
 #ifndef VEC3_H
 #define VEC3_H
+#include "rtweekend.h"
 #include <cmath>
 #include <iostream>
 
@@ -36,6 +37,14 @@ public:
   double length_squared() const {
     return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
   }
+
+  inline static vec3 random() {
+    return vec3(random_double(), random_double(), random_double());
+  }
+  inline static vec3 random(double min, double max) {
+    return vec3(random_double(min, max), random_double(min, max),
+                random_double(min, max));
+  }
 };
 
 // Utility Functions
@@ -64,6 +73,15 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
               u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 inline vec3 unit_vector(vec3 v) { return v / v.length(); }
+
+vec3 random_in_unit_sphere() {
+  while (true) {
+    auto p = vec3::random(-1, 1);
+    if (p.length_squared() >= 1)
+      continue;
+    return p;
+  }
+}
 
 // Type Alias
 using point3 = vec3;
